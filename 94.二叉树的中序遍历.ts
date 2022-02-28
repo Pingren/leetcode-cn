2de4dca412f164e7117c7874e56775c2beaf051f
Pingren <5123601+Pingren@users.noreply.github.com>
2022-02-28 20:57:21 +0800

    solution: 94 (mock callStack)

--- 94.二叉树的中序遍历.ts
+++ 94.二叉树的中序遍历.ts
@@ -19,7 +19,55 @@
  * }
  */
 
+/**@description idea from http://jyywiki.cn/OS/2022/slides/2.slides#/2/3/0 */
 function inorderTraversal(root: TreeNode | null, result = []): number[] {
+  const callStack = []
+  let retrunValue
+  callStack.push({
+    pc: 0,
+    root,
+    result
+  })
+
+  function handleStack(pc, root, result) {
+    if (pc === 0) {
+      if (!root) {
+        callStack.pop()
+        return result
+      }
+    }
+    else if(pc === 1) {
+      callStack.push({
+        pc: 0,
+        root: root.left,
+        result: result
+      })
+    }
+    else if(pc === 2) {
+      result.push(root.val)
+    }
+    else if(pc === 3) {
+      callStack.push({
+        pc: 0,
+        root: root.right,
+        result: result
+      })
+    }
+    else if(pc === 4) {
+      callStack.pop()
+      return result
+    }
+  }
+
+  while (callStack[callStack.length - 1]) {
+    const f = callStack[callStack.length - 1]
+    retrunValue = handleStack(f.pc, f.root, f.result)
+    f.pc = f.pc + 1
+  }
+  return retrunValue
+}
+
+function inorderTraversalR(root: TreeNode | null, result = []): number[] {
   if (!root) {
     return result
   }
